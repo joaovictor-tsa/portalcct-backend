7a85a47ae6e3459d0f088519caa203ce65413f50
joaovictor-tsa <joaovictor@tsacargo.com.br>
2026-07-28 12:07:05 -0300

    remove: archives

--- backend/middlewares/adminMiddleware.js
+++
@@ -1,6 +0,0 @@
-export function adminMiddleware(req, res, next) {
-  if (req.userRole !== "admin") {
-    return res.status(403).json({ erro: "Acesso restrito a administradores." });
-  }
-  next();
-}
